f678dcc27c5b269d3eaf46ed7f9007da52ecd5e7
DESKTOP-9QFP64K\USTC <airocean@mail.ustc.edu.cn>
2022-09-28 15:14:13 +0800

    Using time slice to achieve animation, however not clever

--- e.py
+++ e.py
@@ -10,16 +10,19 @@ v_z=0
 x=0
 y=0
 z=0
-d_t=0.001
-m=1
-q=1
-for t_0 in range(1,50000):
+d_t=1e-7
+m=9.1e-31
+q=1.6e-19
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
+for t_0 in range(1,500000):
     # 时间使用t
     t=d_t*t_0
     b_x=0
     b_y=0
-    b_z=1
-    e_x=1
+    b_z=1e-6
+    e_x=1e-6
     e_y=0
     e_z=0
     x+=(v_x*d_t)
@@ -34,12 +37,12 @@ for t_0 in range(1,50000):
     v_x+=(f_x*d_t/m)
     v_y+=(f_y*d_t/m)
     v_z+=(f_z*d_t/m)
+    figure = ax.plot(x_data, y_data, z_data, c='r')
+    plt.pause(1e-100) #利用时间切片解决这个问题，十分机械、智障的方式
 # print(v_y)
 # plt.plot(x_data,y_data)
-fig = plt.figure()
-ax = fig.gca(projection='3d')
 
-figure = ax.plot(x_data, y_data, z_data, c='r')
+# figure = ax.plot(x_data, y_data, z_data, c='r')
 ax.set_xlabel("X/m")
 ax.set_ylabel("Y/m")
 ax.set_zlabel("Z/m")
